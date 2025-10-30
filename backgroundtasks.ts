84c67c9f1b790bf983022515ad90773ca828af7c
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-10-30 00:54:30 -0300

    create function to started background service

--- backgroundtasks.ts
+++ backgroundtasks.ts
@@ -6,13 +6,51 @@ const BACKGROUND_TASK_IDENTIFIER = "fetch-info-tasks";
 const MINIMUM_INTERVAL = 15;
 const INFO_HISTORY_KEY = "@info_history";
 const MAX_INFO_HISTORY = 10;
+const API_URL = "https://zenquotes.io/api/random"
 
 
 export type Info = {
-    title: string;
-    description: string;
-    author: string;
+    q: string;
+    a: string;
+    h: string;
     timestamp: number;
 }
 
 type InfoHistory = Info[]
+
+export const initializeBackgroundTask = async (innerAppMountedPromise: Promise<void>) => {
+    TaskManager.defineTask(BACKGROUND_TASK_IDENTIFIER, async () => {
+        console.log("Task started")
+        await innerAppMountedPromise;
+
+        try {
+            const response = await fetch(API_URL);
+            const infos: Info[] = await response.json();
+
+            if(infos && infos.length > 0){
+                await storeInfoHistory(infos[0])
+            }
+        } catch (error) {
+            console.log("Error fetching Info: ", error)
+        }
+        
+
+        console.log("Task done ")
+    })
+}
+
+async function storeInfoHistory(info: Info){
+    const history_info = await AsyncStorage.getItem(INFO_HISTORY_KEY);
+    const history: InfoHistory = history_info ? JSON.parse(history_info) : [] ;
+
+    const newInfo = {
+        ...info,
+        timestamp: Date.now()
+    }
+
+    const updateInfo = [newInfo, ...history].slice(0, MAX_INFO_HISTORY);
+
+    await AsyncStorage.setItem(INFO_HISTORY_KEY, JSON.stringify(updateInfo));
+
+    return updateInfo;
+}
